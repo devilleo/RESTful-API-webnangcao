b63b9fc8501a462555338943a462de30165e1813
Kha Leo <lexuankha2409@gmail.com>
2019-10-17 10:44:46 +0700

    change link db

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 //Set up mongoose connection
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb://localhost:27017/mydb';
+const mongoDB = 'mongodb+srv://lexuankha2409:123456kha@cluster0-bezjf.mongodb.net/mydb?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 
