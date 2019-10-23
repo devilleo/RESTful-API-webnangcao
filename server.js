a4ec38de20efb00996e38a7ba9150c50e3a0633f
devilleo <44858821+devilleo@users.noreply.github.com>
2019-10-23 14:56:47 +0700

    apply Access-Control-Allow-Headers

--- server.js
+++ server.js
@@ -24,6 +24,11 @@ app.use(cookieParser());
 app.get('/', function(req, res){
 res.json({"1612278" : "Build REST API with node.js"});
 });
+app.use(function(req, res, next) {
+    // res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 // public route
 app.use('', index);
