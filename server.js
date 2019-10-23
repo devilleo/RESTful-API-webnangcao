6df30edfef1641d7117c5b6a72a3265318a3a82d
devilleo <44858821+devilleo@users.noreply.github.com>
2019-10-23 16:14:37 +0700

    change ACAO

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/', function(req, res){
 res.json({"1612278" : "Build REST API with node.js"});
 });
 app.use(function(req, res, next) {
-    // res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
