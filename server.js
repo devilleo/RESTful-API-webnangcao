62ce798fadbe706d07e3345615823d1e4be15f1a
Kha Leo <lexuankha2409@gmail.com>
2019-10-30 00:27:36 +0700

    add secret_token into 'Access-Control-Allow-Headers'

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ res.json({"1612278" : "Build REST API with node.js"});
 });
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, secret_token");
     next();
   });
 
